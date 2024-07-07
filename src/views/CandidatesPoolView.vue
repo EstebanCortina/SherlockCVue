5e34a7700fd2d7178ad59a4bb30aedff609d2739
Esteban Cortina <83780535+EstebanCortina@users.noreply.github.com>
2024-07-06 23:42:33 -0600

    Add: LoginView & router (#6)
    * Feat[#4]: Add Vue router for view navigation
    
    * Feat[#4]: Add components refactor
    
    * Feat[#4]: Add LoginView
    
    * Feat[#4]: Add login logic with axios

---
+++ src/views/CandidatesPoolView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import CandidatesPoolCom from '@/components/CandidatesAnalysisProcess/CandidatesPoolCom.vue'
+import HeaderCom from '@/components/Basics/HeaderCom.vue'
+
+</script>
+
+<template>
+  <div class="main-body">
+    <HeaderCom />
+    <CandidatesPoolCom />
+  </div>
+</template>
+
+<style scoped>
+
+.main-body {
+  color: var(--secondary-color);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,164 @@
+<script setup lang="ts">
+import axios from 'axios'
+import FormCom from '@/components/Basics/FormCom.vue'
+import AcceptBtnCom from '@/components/Basics/AcceptBtnCom.vue'
+import GapUtil from '@/components/utils/GapUtil.vue'
+import { ref } from 'vue'
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const apiUrl = import.meta.env.VITE_API_URL
+const email = ref('')
+const password = ref('')
+
+function login() {
+  axios.post(`${apiUrl}/login`,
+    {
+      'email': email.value,
+      'password': password.value
+    }).then(response => {
+    console.log(response.data)
+    if (response.status === 200) {
+      router.push({ name: 'CandidatesPool' })
+    }
+
+  })
+    .catch(error => {
+      alert(error.response.data.message)
+    })
+}
+
+</script>
+
+<template>
+  <div class="main-body">
+    <div class="overlay">
+      <FormCom form-title="Iniciar sesión">
+        <template #form-fields>
+          <GapUtil gap="16px">
+            <div class="form-group">
+              <label for="email">Correo:</label>
+              <input name="email" type="email" v-model="email" />
+            </div>
+            <div class="form-group">
+              <label for="password">Contraseña:</label>
+              <input name="password" type="password" v-model="password" />
+            </div>
+          </GapUtil>
+        </template>
+
+        <template #form-buttons>
+          <GapUtil gap="16px">
+            <AcceptBtnCom @click="login" :is-default="false">Login</AcceptBtnCom>
+            <AcceptBtnCom :is-default="false">Crear cuenta</AcceptBtnCom>
+          </GapUtil>
+        </template>
+
+        <template #form-footer>
+          <GapUtil gap="2px">
+            <svg height="2" width="100%">
+              <line class="divider" x1="0" y1="0" x2="100%" y2="0" />
+            </svg>
+            <p style="font-weight: 300; line-height: 150%; text-align: center; color: #828282">
+              Al crear una cuenta, aceptas nuestros
+              <b style="color: var(--details-color); font-weight: 300">Términos de Servicio</b>
+              y la <b style="color: var(--details-color); font-weight: 300">Política de Privacidad</b>
+            </p>
+          </GapUtil>
+        </template>
+      </FormCom>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  /* Aplicar desenfoque */
+  /*
+  backdrop-filter: blur(10px);
+   */
+}
+
+.divider {
+  stroke: var(--details-color);
+  stroke-width: 1;
+}
+
+.main-body {
+  background-color: var(--secondary-color);
+  color: var(--details-color);
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  /*
+    background-image: radial-gradient(closest-side, transparent 98%, rgba(0, 0, 0, .3) 99%),
+    radial-gradient(closest-side, transparent 98%, rgba(0, 0, 0, .3) 99%);
+    background-size: 80px 80px;
+    background-position: 0 0, 40px 40px;
+   */
+
+
+  /*
+  --s: 150px;
+  --c: var(--secondary-color);
+  --_s: calc(2 * var(--s)) calc(2 * var(--s));
+  --_g: 35.36% 35.36% at;
+  --_c: #0000 66%, #20222a 68% 70%, #0000 72%;
+  background: radial-gradient(var(--_g) 100% 25%, var(--_c)) var(--s) var(--s)/var(--_s),
+  radial-gradient(var(--_g) 0 75%, var(--_c)) var(--s) var(--s)/var(--_s),
+  radial-gradient(var(--_g) 100% 25%, var(--_c)) 0 0/var(--_s),
+  radial-gradient(var(--_g) 0 75%, var(--_c)) 0 0/var(--_s),
+  repeating-conic-gradient(var(--c) 0 25%, #0000 0 50%) 0 0/var(--_s),
+  radial-gradient(var(--_c)) 0 calc(var(--s) / 2)/var(--s) var(--s) var(--c);
+  background-attachment: fixed;
+  */
+
+}
+
+.form-group {
+  margin-bottom: 0;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5em;
+  font-weight: 250;
+}
+
+input {
+  width: calc(100% - 1em);
+  height: 20px;
+  padding: 0.5em;
+  border: 1px solid #E0E0E0;
+  border-radius: 8px;
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 0 8px var(--details-color);
+  transition: box-shadow 0.3s ease;
+}
+
+button {
+  width: 100%;
+}
+
+</style>
