c1ab10c8bf33f8b4623b66904dfb94cf8cd98472
Esteban Cortina <83780535+EstebanCortina@users.noreply.github.com>
2024-08-30 03:51:28 -0600

    Fix: change WebHsitory to WebHashHistory (#16)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import JobCardsView from '@/views/JobCardsView.vue'
 
 const routes = [
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
